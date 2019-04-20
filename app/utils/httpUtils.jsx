342c464caf923f9968343b716868406ff214008a
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2019-04-20 17:47:11 -0400

    make cosmetic changes

--- app/utils/httpUtils.jsx
+++ app/utils/httpUtils.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// export const host = 'http://localhost:8000/api/v1'
-export const host = 'http://138.197.139.181:8000/api/v1'
+export const host = 'http://localhost:8000/api/v1'
+// export const host = 'http://138.197.139.181:8000/api/v1'
 
 export const request = axios.create({
   baseURL: host,
--- dist/app.9e510e41.js
+++ dist/app.9e510e41.js
@@ -45881,8 +45881,8 @@ var _axios = _interopRequireDefault(require("axios"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// export const host = 'http://localhost:8000/api/v1'
-var host = 'http://138.197.139.181:8000/api/v1';
+var host = 'http://localhost:8000/api/v1'; // export const host = 'http://138.197.139.181:8000/api/v1'
+
 exports.host = host;
 
 var request = _axios.default.create({
